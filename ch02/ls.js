c5e20352a7d95a75fe175fd5e58efcba5e3c74e2
Dan Pitic <dpitic@gmail.com>
2016-09-21 20:14:48 +0800

    Completed http server example. Next NPM.

---
+++ ch02/ls.js
@@ -0,0 +1,7 @@
+// Simple implementation of ls
+
+var fs = require('fs');
+var files = fs.readdirSync('.');
+for (fn in files) {
+  console.log(files[fn])
+}
---
+++ ch02/ls2.js
@@ -0,0 +1,12 @@
+/**
+ * Extension of the ls command to accept a directory to list.
+ */
+
+var fs = require('fs');
+var dir = '.';
+if (process.argv[2])
+    dir = process.argv[2];
+var files = fs.readdirSync(dir);
+for (fn in files) {
+    console.log(files[fn]);
+}
